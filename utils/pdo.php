aebbbfb3a04826aa70554500756e40fa0e2d8f5b
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-10-19 11:39:52 +0200

    appel et lecture du fichier json, nouvelle initialistation de la connexion vers la bdd

--- utils/pdo.php
+++ utils/pdo.php
@@ -1,12 +1,11 @@
 <?php
 
-$serveur = "localhost";
-$dbname = "annuairenws";
-$login = "rachid";
-$password = "rachidroot";
+$configContent = file_get_contents('./utils/config.json');
+$globalConfigs = json_decode($configContent, true);
 
 try {
-    $pdo = new PDO("mysql:host=$serveur;dbname=$dbname", $login, $password, array(
+    $connection = 'mysql:host=' . $globalConfigs["database"]['host'] . ';dbname=' . $globalConfigs['database']["db_name"];
+    $pdo = new PDO($connection, $globalConfigs['database']['user'], $globalConfigs['database']['password'], array(
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
