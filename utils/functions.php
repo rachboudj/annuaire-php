dc155c54ce169381e7491ac5cdbb6603af672eda
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-10-22 00:23:56 +0200

    création de la fonction pour modifier un élève

--- utils/functions.php
+++ utils/functions.php
@@ -2,26 +2,43 @@
 
 function validationTexte($er, $data, $key, $min, $max)
 {
-    if(!empty($data)) {
-        if(mb_strlen($data) < $min) {
-            $er[$key] = 'Il faut minimum '.$min.' caractères.';
-        } elseif(mb_strlen($data) >= $max) {
-            $er[$key] = 'Il faut maximum '.$max.' caractères.';
+    if (!empty($data)) {
+        if (mb_strlen($data) < $min) {
+            $er[$key] = 'Il faut minimum ' . $min . ' caractères.';
+        } elseif (mb_strlen($data) >= $max) {
+            $er[$key] = 'Il faut maximum ' . $max . ' caractères.';
         }
-    } else{
+    } else {
         $er[$key] = 'Veuillez renseigner ce champ.';
     }
     return $er;
 }
 
-function requeteSuppr($id, $pdo) {
-    if (!empty($_GET['id']) && ctype_digit($_GET['id'])) 
-    {
-    $requeteSuppr = "DELETE FROM nouveaux_eleves WHERE id = :id";
-    
-    $query = $pdo->prepare($requeteSuppr);
-    $query->bindValue(':id', $id, PDO::PARAM_INT);
-    $query->execute();
-    header('Location: /annuaire-php/index.php?');
+function requeteSuppr($id, $pdo)
+{
+    if (!empty($_GET['id']) && ctype_digit($_GET['id'])) {
+        $requeteSuppr = "DELETE FROM nouveaux_eleves WHERE id = :id";
+
+        $query = $pdo->prepare($requeteSuppr);
+        $query->bindValue(':id', $id, PDO::PARAM_INT);
+        $query->execute();
+        header('Location: /annuaire-php/listEleve.php?');
     }
 }
+
+function requeteModif($pdo, $nom, $prenom, $age, $diplome, $specialite, $email, $telephone, $id)
+{
+    $sql = "UPDATE nouveaux_eleves SET nom= :nom, prenom= :prenom, age= :age, diplome= :diplome, specialite= :specialite, email= :email, telephone= :telephone WHERE id= :id";
+    $query = $pdo->prepare($sql);
+    $query->bindValue(':nom', $nom, PDO::PARAM_STR);
+    $query->bindValue(':prenom', $prenom, PDO::PARAM_STR);
+    $query->bindValue(':age', $age, PDO::PARAM_STR);
+    $query->bindValue(':diplome', $diplome, PDO::PARAM_STR);
+    $query->bindValue(':specialite', $specialite, PDO::PARAM_STR);
+    $query->bindValue(':email', $email, PDO::PARAM_STR);
+    $query->bindValue(':telephone', $telephone, PDO::PARAM_STR);
+    $query->bindValue(':id', $id, PDO::PARAM_INT);
+    $query->execute();
+    header('Location: /annuaire-php/listEleve.php?');
+}
+
