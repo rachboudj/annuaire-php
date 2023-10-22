313f312ab3c7e6900944524253be2570d04da977
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-10-22 14:22:50 +0200

    suppression du footer

--- index.php
+++ index.php
@@ -5,6 +5,3 @@ require_once "./utils/pdo.php";
 
 <h1>Annuaire de la NWS</h1>
 <button><a href="/annuaire-php/listeEleve.php">Voir la liste des élèves</a></button>
-
-<?php
-include_once "./includes/_footer.php";
